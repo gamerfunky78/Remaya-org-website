46593192ad4cf2b7e701bdb95621517629ab76be
Murchoid <kamaucoder@gmail.com>
2024-05-08 22:01:01 +0300

    Before changing the css to responisve one

---
+++ app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navbarToggle = document.querySelector('.navbar-toggle');
+    const navbar = document.querySelector('.navbar');
+
+    navbarToggle.addEventListener('click', function() {
+        navbar.classList.toggle('collapsed');
+        navbarToggle.classList.toggle('change');
+    });
+});
